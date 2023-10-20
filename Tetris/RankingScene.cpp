05d3101edaf7ede8f505a951ffc57f25f08fc6c4
Tomimura-yoshinori3225 <138739518+Tomimura-yoshinori3225@users.noreply.github.com>
2023-10-20 14:54:54 +0900

    RankingScene
    RankingSceneを書いている途中

--- Tetris/RankingScene.cpp
+++ Tetris/RankingScene.cpp
@@ -25,6 +25,53 @@ typedef struct
 {
 	int rank;                       //ランク
 	char name[RANKING_NAME_LEN];    //名前
-	int scene;                      //スコア4
+	int scene;                      //スコア
 }T_RANKING;
+
+
+typedef struct
+{
+	int x;
+	int y;
+}T_CURSOR;
+
+
+/******************************
+
+*グローバル変数定義
+
+**********************************/
+
+T_RANKING Ranking_Data[RANKING_MAX];
+T_RANKING New_Score;
+int DispMode;
+
+T_CURSOR Cursor;
+int name_num;
+
+
+/*****************************************
+
+*プロトタイプ宣言
+
+******************************************/
+
+void file_read(void);               //ファイルl読み込み
+void file_write(void);              //ファイル書き込み
+void ranking_sort(void);           //ランキングソート処理
+void ranking_input_name(void);      //名前入力処理
+void ranking_input_name_draw(void);   //名前入力描画処理
+
+
+/*********************************
+
+*ランキング画面：初期化処理
+
+*引数：なし
+
+*戻り値：エラー情報（-１：以上有、-１以外：正常終了）
+
+****************************************/
+
+
 
