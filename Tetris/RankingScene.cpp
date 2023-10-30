868f40a65b3aec3fe68a07875db3032d9bf4d53b
Tomimura-yoshinori3225 <138739518+Tomimura-yoshinori3225@users.noreply.github.com>
2023-10-30 23:10:37 +0900

    RankingScene.cpp
    RankingScene.cppを書いている途中

--- Tetris/RankingScene.cpp
+++ Tetris/RankingScene.cpp
@@ -73,5 +73,101 @@ void ranking_input_name_draw(void);   //名前入力描画処理
 
 ****************************************/
 
+int RankinguScene_Initialize(void)
+{
+	int ret = 0;
+	file_read();
+	switch (DispMode)
+	{
+	case RANKING_INPUT_MODE:
+		Cursor.x = 0;
+		Cursor.y = 0;
+		name_num = 0;
+		break;
+
+	case RANKING_DISP_MODE:
+	default:
+		break;
+	}
+	return ret;
+}
+
+
+/*******************************
+
+*ランキング画面：更新処理
+
+*引数：なし
+
+*戻り値：なし
+
+*************************************/
+
+void RankingScene_Update(void)
+{
+	switch (DispMode)
+	{
+	case RANKING_INPUT_MODE:
+		ranking_input_name();
+		break;
+
+	case RANKING_DISP_MODE:
+	default:
+		if (GetButtonDown(XINPUT_BUTTON_B))
+		{
+			Change_Scene(E_TITLE);
+		}
+		break;
+	}
+}
+
+
+/**********************************
+
+*ランキング画面：描画処理
+
+*引数：なし
+
+*戻り値：なし
+
+***************************************/
+
+void RankingScene_Draw(void)
+{
+	int i;
+
+	switch (DispMode)
+	{
+	case RANKING_INPUT_MODE:
+		ranking_input_name_draw();
+		break;
+
+	case RAKING_DISP_MODE:
+	default:
+		for (i = 0; i < RANKING_MAX; i++)
+		{
+			DrawFormatString(20, 10 + (i * 25), GetColor(255, 255, 255), "%2d,%10s,%10d", Ranking_Date[i].rank,
+				Ranking_Date[i].name, Ranking_Date[i].scone);
+		}
+		break;
+
+	}
+}
+
+/*******************************
+
+*ランキング画面：画面変更処理
+
+*引数：なし
+
+*戻り値：なし
+
+**************************************/
+
+void Set_RankingMode(int mode)
+{
+	DispMode = mode;
+}
+
 
 
