b398f71aef12cc544e7c9019f9e3301b90012971
Tomimura-yoshinori3225 <138739518+Tomimura-yoshinori3225@users.noreply.github.com>
2023-11-30 15:00:42 +0900

    Block.h
    Block.hを書いている途中

--- Tetris/Block.cpp
+++ Tetris/Block.cpp
@@ -108,4 +108,188 @@ const int C_BLOCK_TABLE[BLOCK_TYPE_MAX][BLOCK_TROUT_SIZE][BLOCK_TROUT_SIZE] = {
 int BlockImage[E_BLOCK_IMAGE_MAX];                     //ブロック画像
 BLOCK_STATE Field[FIELD_HEIGHT][FIELD_WIDTH];          //フィールド配列
 BLOCK_STATE Next[BLOCK_TROUT_SIZE][BLOCK_TROUT_SIZE];  //待機状態のブロック
-BLOCK_STATE Stock
+BLOCK_STATE Stock[BLOCK_TROUT_SIZE][BLOCK_TROUT_SIZE]; //ストックのブロック
+BLOCK_STATE DropBlock[BLOCK_TROUT_SIZE][BLOCK_TROUT_SIZE]; //落ちるブロック
+int DropBlock_X;                                       //落ちるブロックのX座標
+int DropBlock_Y;                                       //落ちるブロックのY座標
+int WaitTime;           //待機時間
+int Stock_Flg;          //ストックフラグ
+int DeleteLine;         //消したラインの数
+int SoundEffect[3];     //SE
+
+
+
+/****************************************
+
+*プロトタイプ宣言
+
+****************************************/
+
+
+void create_field(void);                   //フィールドの生成処理
+void create_block(void);                   //ブロックの生成処理
+void move_block(void);                     //ブロックの移動処理
+void change_block(void);                    //ストック交換処理
+void turn_block(int clockwise);            //ブロック回転処理
+int check_overlap(int x, int y);           //範囲外チェック処理
+void lock_block(int x, int y);        //着地したブロックを固定済みに変更する処理
+void check_line(void);                //ブロックの横一列確認処理
+
+
+
+/*******************************************
+
+*ブロック機能：初期化処理
+
+*引数：なし
+
+*戻り値：エラー情報（-1:異常、それ以外：正常）
+
+********************************************/
+
+int Block_Initialize(void)
+{
+	int ret = 0;           //戻り値
+	int i = 0;
+
+	//ブロックの画像の読み込み
+	ret = LoadDivGraph("image/blocl.png", E_BLOCK_IMAGE_MAX, 10, 1, BLOCK_SIZE,
+		BLOCK_SIZE, BlockImage);
+
+	//SEの読み込み
+	SoundEffect[0] = LoadSoundMem("sound/SE3.mp3");
+	SoundEffect[1] = LoadSoundMem("sound/SE4.mp3");
+	SoundEffect[2] = LoadSoundMem("sound/SE5.wav");
+
+	//音量の調整
+	ChangeVolumeSoundMem(150, SoundEffect[0]);
+	ChangeVolumeSoundMem(150, SoundEffect[1]);
+	ChangeVolumeSoundMem(130, SoundEffect[2]);
+
+	//フィールドの生成
+	create_field();
+
+	//ブロック生成
+	create_block();
+	create_block();
+
+	//待機時間の初期化
+	WaitTime = 0;
+
+	//ストックフラグの初期化
+	Stock_Flg = FALSE;
+
+	//生成フラグの初期化
+	Generate_Flg = TRUE;
+
+	//消したラインの数初期化
+	DeleteLine = 0;
+
+	//エラーチェック
+	for (i = 0; i < 3; i++)
+	{
+		if (SoundEffect[i] == -1)
+		{
+			ret = -1;
+			break;
+		}
+	}
+	return ret;
+}
+
+
+/*********************************
+
+*ブロック機能：更新処理
+
+*引数：なし
+
+*戻り値：なし
+
+***********************************/
+
+void Block_Update(void)
+{
+	//ブロックの移動処理
+	move_block();
+
+	//ブロックのストック
+	if ((GetButtonDown(XINPUT_BUTTON_LEFT_SHOULDER) == TRUE) ||
+		(GetButtonDown(XINPUT_BUTTON_RIGHT_SHOULDER) == TRUE))
+	{
+		//生成可能であれば
+		if (Generate_Flg == TRUE)
+		{
+			change_block();       //ストック交換処理
+			//ブロックの回転を正位置にする
+		}
+	}
+
+	//ブロックの回転（反時計周り）
+	if ((GetButtonDown(XINPUT_BUTTON_B) == TRUE) ||
+		(GetButtonDown(XIPUT_BUTTON_X) == TRUE))
+	{
+		turn_block(TURN_CROCKWICE);
+	}
+	
+	//落下処理
+	WaitTime++;              //カウンタの更新
+	if (WaitTime > DROP_SPEED)
+	{
+		if (check_overlap(DropBlock_X, DropBlock_Y + 1) == TRUE)
+		{
+			DropBlock_Y++;
+		}
+		else
+		{
+			//ブロックの固定
+			lock_block(DropBlock_X, DropBlock_Y);
+			//ブロックの消去とブロックを下す処理
+			check_line();
+			//新しいブロックの生成
+			create_block();
+		}
+		//カウンタの初期化
+		WaitTime = 0;
+	}
+}
+
+/***********************************
+
+*ブロック機能：描画処理
+
+*引数：なし
+
+*戻り値：なし
+
+**************************************/
+
+void Block_Draw(void)
+{
+	int i, j;          //ループカウンタ
+
+	//フィールドのブロックを描画
+	for (i = 0; i < FIELD_HEIGHT; i++)
+	{
+		for (j = 0; j < FIELD_WIDTH; j++)
+		{
+			if (Field[i][j] != E_BLOCK_WALL)
+			{
+				DrawGraph(j * BLOCK_SIZE, i * BLOCK_SIZE, BlockImage[Field[i][j]],
+					TRUE);
+			}
+		}
+	}
+	//次のブロックとストックされたブロックを描画
+	for (i = 0; i < BLOCK_TROUT_SIZE; i++)
+	{
+		for (j = 0; j < BLOCK_TROUT_SIZE; j++)
+		{
+			//次のブロックを描画
+			DrawGraph(BLOCK_SIZE * j + BLOCK_NEXT_POS_X, BLOCK_SIZE * i +
+				BLOCK_NEXT_POS_Y, BlockImage[Next[i][j]], TRUE);
+			//ストックされたブロックを描画
+
+		}
+	}
+}
